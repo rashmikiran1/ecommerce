7cf34d36e5d38049ae87ea41911239355d0e1f73
rashmikiran1 <rashmikiran55@gmail.com>
2023-08-18 22:37:39 +0530

    fetch movie

--- src/components/home.js
+++ src/components/home.js
@@ -1,48 +1,40 @@
-import React from "react";
+import React, { useState} from "react";
 import classes from '../style/home.module.css';
-const Home = ()=> {
-    return (
-        <div>
-            <div className={classes.album}><span>GET OUR LATEST ALBUM</span></div>
-            <h3>TOURS</h3>
-            <p className={classes.item}>
-  <span className={classes.itemSpan}>JULY 16</span>
-  <span className={classes.itemSpan}>DETROIT, MI</span>
-  <span className={classes.itemSpan}>DTE ENERGY MUSIC THEATRE</span>
-  <span className={classes.itemSpan}><button>buy tickets</button></span>
-</p>
-<p className={classes.item}>
-  <span className={classes.itemSpan}>JULY 16</span>
-  <span className={classes.itemSpan}>DETROIT, MI</span>
-  <span className={classes.itemSpan}>DTE ENERGY MUSIC THEATRE</span>
-  <span className={classes.itemSpan}><button>buy tickets</button></span>
-</p>
-<p className={classes.item}>
-  <span className={classes.itemSpan}>JULY 16</span>
-  <span className={classes.itemSpan}>DETROIT, MI</span>
-  <span className={classes.itemSpan}>DTE ENERGY MUSIC THEATRE</span>
-  <span className={classes.itemSpan}><button>buy tickets</button></span>
-</p>
-<p className={classes.item}>
-  <span className={classes.itemSpan}>JULY 16</span>
-  <span className={classes.itemSpan}>DETROIT, MI</span>
-  <span className={classes.itemSpan}>DTE ENERGY MUSIC THEATRE</span>
-  <span className={classes.itemSpan}><button>buy tickets</button></span>
-</p>
-<p className={classes.item}>
-  <span className={classes.itemSpan}>JULY 16</span>
-  <span className={classes.itemSpan}>DETROIT, MI</span>
-  <span className={classes.itemSpan}>DTE ENERGY MUSIC THEATRE</span>
-  <span className={classes.itemSpan}><button>buy tickets</button></span>
-</p>
-<p className={classes.item}>
-  <span className={classes.itemSpan}>JULY 16</span>
-  <span className={classes.itemSpan}>DETROIT, MI</span>
-  <span className={classes.itemSpan}>DTE ENERGY MUSIC THEATRE</span>
-  <span className={classes.itemSpan}><button>buy tickets</button></span>
-</p>
 
-        </div>
-    )
+const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  const [fetching, setFetching] = useState(false);
+
+  const fetchMovies = async () => {
+    try {
+      setFetching(true);
+      const response = await fetch('https://swapi.dev/api/films');
+      const data = await response.json();
+      setMovies(data.results);
+      setFetching(false);
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+      setFetching(false);
+    }
+  };
+  return (
+    <div>
+      <div className={classes.album}><span>GET OUR LATEST ALBUM</span></div>
+      <h3 className={classes.tour}>TOURS</h3>
+      <button onClick={fetchMovies} disabled={fetching} className={classes.tour}>
+        {fetching ? 'Fetching...' : 'Fetch Movies'}
+      </button>
+      {movies.map((movie, index) => (
+        <p className={classes.item} key={index}>
+          <span className={classes.itemSpan}>{movie.title}</span>
+          <span className={classes.itemSpan}>{movie.director}</span>
+          <span className={classes.itemSpan}>{movie.release_date}</span>
+          <span className={classes.itemSpan}><button>buy tickets</button></span>
+        </p>
+      ))}
+    </div>
+  );
 }
+
 export default Home;
