d3581c3e06a5f042e77bac1682b176fb93f5b750
rashmikiran1 <rashmikiran55@gmail.com>
2023-08-18 23:54:55 +0530

    movielist

--- src/components/home.js
+++ src/components/home.js
@@ -1,30 +1,27 @@
-import React, { useState} from "react";
+import React, { useState, useEffect } from "react";
 import classes from '../style/home.module.css';
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
 
-  const [fetching, setFetching] = useState(false);
-
-  const fetchMovies = async () => {
-    try {
-      setFetching(true);
-      const response = await fetch('https://swapi.dev/api/films');
-      const data = await response.json();
-      setMovies(data.results);
-      setFetching(false);
-    } catch (error) {
-      console.error('Error fetching movies:', error);
-      setFetching(false);
+  useEffect(() => {
+    async function fetchMovies() {
+      try {
+        const response = await fetch('https://swapi.dev/api/films');
+        const data = await response.json();
+        setMovies(data.results);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
     }
-  };
+
+    fetchMovies();
+  }, []);
+
   return (
     <div>
       <div className={classes.album}><span>GET OUR LATEST ALBUM</span></div>
-      <h3 className={classes.tour}>TOURS</h3>
-      <button onClick={fetchMovies} disabled={fetching} className={classes.tour}>
-        {fetching ? 'Fetching...' : 'Fetch Movies'}
-      </button>
+      <h3 className={classes.tour}>MOVIES LIST</h3>
       {movies.map((movie, index) => (
         <p className={classes.item} key={index}>
           <span className={classes.itemSpan}>{movie.title}</span>
